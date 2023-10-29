6254fca25997b1bfc4a4247f1c9a713a2a54f3f1
Anand Kumar Mishra <ctfu.anand@outlook.com>
2023-10-29 15:47:04 +0530

    First TDD (to be removed later)

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator functions
 def add(x,y):
     """Add two numbers together"""
     return x + y
+
+def subtract(x,y):
+    """Subtract x from y and return value"""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -16,3 +16,8 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)    
