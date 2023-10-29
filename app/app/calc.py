f68df64f560c69a3bfd3a16b354e68754a9ea20d
Anand Kumar Mishra <ctfu.anand@outlook.com>
2023-10-29 15:43:20 +0530

    sample unit test (to be removed later)

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Add two numbers together"""
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """
+    Test the calc module
+
+    """
+
+    def test_add_number(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
