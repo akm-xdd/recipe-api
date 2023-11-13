24372a3c699c01afd5a91193bb5e2335a08ba8bf
Anand Kumar Mishra <ctfu.anand@outlook.com>
2023-11-13 12:24:24 +0530

    User Model Created

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,46 @@
-from django.db import models # noqa
+"""
+Database models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, 
+                                        BaseUserManager, 
+                                        PermissionsMixin)
+
+class UserManager(BaseUserManager):
+    """Manager for user profiles"""
+    
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+        
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user     
+    
+    def create_superuser(self, email, password):
+        """Create and save a new superuser"""
+        
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) # can login
+    is_staff = models.BooleanField(default=False) # is staff user
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
